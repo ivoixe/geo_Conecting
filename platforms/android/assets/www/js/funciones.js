825d8a6dc70672f8b597ae71bced05f3875c1bf8
ivoixe <ivonne@prismacm.com>
2018-08-10 11:01:45 +0200

    probando

--- platforms/android/assets/www/js/funciones.js
+++ platforms/android/assets/www/js/funciones.js
@@ -15,7 +15,7 @@
 		function onSuccess(position) {
 				var element = document.getElementById('geolocation');
 				initialize(position.coords.latitude,position.coords.longitude);				
-				
+				alert(position.coords.latitude ,position.coords.longitude);
 				return position;
 		}
 			
