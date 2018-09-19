825cdd14d6dc60a44ec323c51cedcffa410547bc
ivoixe <ivonne@prismacm.com>
2018-09-19 12:08:18 +0200

    fallo al build background mode

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,23 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "id": "cordova-plugin-device.device",
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "pluginId": "cordova-plugin-device",
-        "clobbers": [
-            "device"
-        ]
-    },
-    {
-        "id": "cordova-plugin-background-mode.BackgroundMode",
-        "file": "plugins/cordova-plugin-background-mode/www/background-mode.js",
-        "pluginId": "cordova-plugin-background-mode",
-        "clobbers": [
-            "cordova.plugins.backgroundMode",
-            "plugin.backgroundMode"
-        ]
-    },
-    {
         "id": "cordova-plugin-globalization.GlobalizationError",
         "file": "plugins/cordova-plugin-globalization/www/GlobalizationError.js",
         "pluginId": "cordova-plugin-globalization",
@@ -85,8 +68,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-device": "2.0.2",
-    "cordova-plugin-background-mode": "0.7.2",
     "cordova-plugin-globalization": "1.11.0",
     "org.apache.cordova.geolocation": "0.3.6",
     "com.pbakondy.sim2": "1.0.16",
