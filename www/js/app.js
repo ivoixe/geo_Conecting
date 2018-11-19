6506991c8068b7567a4cfd7ac0988d18bce8e18a
ivoixe <ivonne@prismacm.com>
2018-11-19 09:28:10 +0100

    ss

--- www/js/app.js
+++ www/js/app.js
@@ -1,11 +1,51 @@
-document.addEventListener('init', function(event) {
-  var page = event.target;
 
-  if (page.id === 'page1') {
-    page.querySelector('#push-button').onclick = function() {
-      document.querySelector('#myNavigator').pushPage('page2.html', {data: {title: 'Page 2'}});
-    };
-  } else if (page.id === 'page2') {
-    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
-  }
-});
+angular.module('App', [])
+    .controller('AppCtrl', function ($scope) {
+
+        $scope.model = 0;
+
+        $scope.initSlider = function () {
+            $(function () {
+                // wait till load event fires so all resources are available
+                $scope.$slider = $('#slider').slider({
+                    slide: $scope.onSlide
+                });
+            });
+
+            $scope.onSlide = function (e, ui) {
+                $scope.model = ui.value;
+                $scope.$digest();
+            };
+        };
+
+        $scope.initSlider();
+    });
+
+
+angular.module('App', [])
+    .directive('slider', function (DataModel) {
+        return {
+            restrict: 'A',
+            scope: true,
+            controller: function ($scope, $element, $attrs) {
+                $scope.onSlide = function (e, ui) {
+                    $scope.model = ui.value;
+                    // or set it on the model
+                    // DataModel.model = ui.value;
+                    // add to angular digest cycle
+                    $scope.$digest();
+                };
+            },
+            link: function (scope, el, attrs) {
+
+                var options = {
+                    slide: scope.onSlide
+                };
+
+                // set up slider on load
+                angular.element(document).ready(function () {
+                    scope.$slider = $(el).slider(options);
+                });
+            }
+        }
+    });
