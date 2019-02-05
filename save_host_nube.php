b310b500ebbe20440932bd5e3934fc78a09fd1eb
ivoixe <ivonne@prismacm.com>
2019-02-05 15:58:45 +0100

    no estaba mal
    era por permisos de localizacion

--- save_host_nube.php
+++ save_host_nube.php
@@ -11,21 +11,27 @@
  	
 	$usuario = (!empty($_POST['usuario']))? $_POST['usuario']:''; 
 	$pass = (!empty($_POST['password']))? $_POST['password']:'';
+
+
 	$conn = connect_db($db);
 	$sql= 'SELECT * FROM usuario WHERE usuario = "'.$usuario.'" AND password="'.$pass.'"';
 	$bd_usuarios	= make_query($conn,$sql);
+
 	$mensaje=array();
 	$mensaje['error']= "sin nada";
 	if(!empty($bd_usuarios)){
-		if($usuario ==  $bd_usuarios[0]['usuario'] &&  $pass == $bd_usuarios[0]['password']){
+		if($usuario ==  $bd_usuarios[0]['usuario'] &&  $pass == $bd_usuarios[0]['password'] && !empty($token)){
 
 			$hora = (!empty($_POST['hora']))? $_POST['hora']:0;
 			$lat = (!empty($_POST['lat']))? $_POST['lat'] : 0;
 			$log = (!empty($_POST['log']))?$_POST['log'] :0;
 			$latlog= $lat.','.$log;
 			if(!empty($hora) && !empty($latlog) && $latlog !=0){
-				  $sql= 'INSERT INTO ubicaciones (hora,latlog,user) VALUES ("'.$hora.'","'.$latlog.'","'.$usuario.'") ';
+				  $sql= 'INSERT INTO ubicaciones (hora,latlog,usuario) VALUES ("'.$hora.'","'.$latlog.'","'.$usuario.'") ';
 		  			save_data($conn,$sql);
+                $sql= 'UPDATE usuario  SET token="'.$token.'" where  id='.$bd_usuarios[0]['id'];
+                save_data($conn,$sql);
+
 		  	//descargamos los datos en la app para que los guarde en el movil
 				$sql= 'SELECT * FROM usuario_horarios WHERE id_usuario= "'.$bd_usuarios[0]['id'].'" ORDER BY horario_entrada ASC';
 				$UsuarioHorario	= make_query($conn,$sql);
